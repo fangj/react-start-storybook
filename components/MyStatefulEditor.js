587196d8427fb39f0cd9695ea557231fd2d1ec75
Fang Jian <fangjianmail@126.com>
2016-12-05 14:11:45 +0800

    rte 中文菜单

--- components/MyStatefulEditor.js
+++ components/MyStatefulEditor.js
@@ -23,10 +23,30 @@ export default class MyStatefulEditor extends Component {
   };
 
   render () {
+     const toolbarConfig = {
+    // Optionally specify the groups to display (displayed in the order listed).
+    display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'LINK_BUTTONS', 'BLOCK_TYPE_DROPDOWN', 'HISTORY_BUTTONS'],
+    INLINE_STYLE_BUTTONS: [
+      {label: '加粗', style: 'BOLD'},
+      {label: '斜体', style: 'ITALIC'},
+      {label: '下划线', style: 'UNDERLINE'}
+    ],
+    BLOCK_TYPE_DROPDOWN: [
+      {label: '正文', style: 'unstyled'},
+      {label: '大标题', style: 'header-one'},
+      {label: '中标题', style: 'header-two'},
+      {label: '小标题', style: 'header-three'}
+    ],
+    BLOCK_TYPE_BUTTONS: [
+      {label: '无序列表', style: 'unordered-list-item'},
+      {label: '有序列表', style: 'ordered-list-item'}
+    ]
+  };
     return (
       <RichTextEditor
         value={this.state.value}
         onChange={this.onChange}
+        toolbarConfig={toolbarConfig} 
       />
     );
   }

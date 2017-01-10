452f8348881e3434a1982740d06b05b77c84488a
Fang Jian <fangjianmail@126.com>
2017-01-10 17:32:24 +0800

    menu

--- components/list/Menu.js
+++ components/list/Menu.js
@@ -8,8 +8,8 @@ export default class Menu extends React.Component {
     idx: React.PropTypes.number, //菜单块在父列表中的位置
   };
   static defaultProps = {
-    pid:"pid"
-    idx: 0,
+    pid:"pid",
+    idx: 0
   };
 
   constructor(props) {

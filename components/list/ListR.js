5ead73ab113526acf7c9ae89773bf2f8a22c8345
Fang Jian <fangjianmail@126.com>
2017-01-19 14:18:18 +0800

    clean code

--- components/list/ListR.js
+++ components/list/ListR.js
@@ -9,13 +9,8 @@ import agent from "axios";
 import List from "./List";
 const getData=(url)=>agent.get(url).then(res=>res.data);
 
-// const ListR=(url)=>promiseFactoryView(()=>getData(url),{value:"items"})(List);
 const ListR=({url,...others})=>{
   const InjectItems=promiseFactoryView(()=>getData(url),{value:"items"});
-  // return (props)=>{
-  //   const V=InjectItems(<div>xxx</div>);
-  //   return <div>xxx</div>;
-  // };
   const V=InjectItems(List);
   return <V {...others}/>;
 };
